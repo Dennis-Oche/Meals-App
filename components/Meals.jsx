7bb8f6e594d754c85f5533b7caa373de9287b349
unknown <okohdennisoche@gmail.com>
2022-10-08 13:00:27 +0100

    first commit

---
+++ components/Meals.jsx
@@ -0,0 +1,44 @@
+import { useGlobalContext } from '../context';
+import {BsHandThumbsUp} from 'react-icons/bs'
+
+const Meals = () => {
+  const {loading, meals, selectMeal, addToFavorites, removeFromFavorites} = useGlobalContext();
+
+  
+  if (loading) {
+    return (      
+      <section className="section">
+        <h4>Loading...</h4>
+      </section> 
+    )
+  }
+  
+  if (meals.length < 1) {
+    return (
+      <section className="section">      
+        <h4>No meals attached to your search term. Please try again.</h4>
+      </section>
+    )
+    
+  }
+  
+  return (
+    <section className="section-center">
+      {
+        meals.map((singleMeal) => {
+          const {idMeal, strMeal: title, strMealThumb: image} = singleMeal;
+          return <article key={idMeal} className="single-meal">
+            <img src={image} alt={title} className="img" onClick={() => selectMeal(idMeal)}/>
+            <footer>
+              <h5>{title}</h5>
+              <button className='like-btn' onClick={() => addToFavorites(idMeal)}><BsHandThumbsUp/></button>
+            </footer>
+          </article>
+        })
+      }
+    </section>
+  )
+
+}
+
+export default Meals;
---
+++ components/Search.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useGlobalContext } from '../context';
+
+const Search = () => {
+  const [text, setText] = useState("");
+  const { setSearchTerm, fetchRandomMeal } = useGlobalContext();
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text) {
+      setSearchTerm(text);
+    }
+  };
+
+  const handleRandomMeal = () => {
+    setSearchTerm("");
+    setText("");
+    fetchRandomMeal();
+  }
+
+  return (
+    <header className='search-container'>
+      <form onSubmit={handleSubmit}>
+        <input className='form-input' type='text' placeholder='type favorite meal' onChange={handleChange} value={text} />
+        <button className='btn' type='submit'>Search</button>
+        <button className='btn btn-hipster' type='button' onClick={handleRandomMeal}>Surprise me!</button>
+      </form>
+    </header>
+  )
+}
+
+export default Search;
